0645c04255a09a9b1537da221ced5ca990def130
tngyn <timngyn@gmail.com>
2015-05-12 18:08:10 -0700

    Setting up node project

---
+++ app.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var session = require('express-session');
+var cookieParser = require('cookie-parser');
+var path = require('path');
+var handlebars = require('express-handlebars');
+var logger = require('morgan');
+var bodyParser = require('body-parser');
+var async = require('async');
+var app = express();
+
+// Configure the template engine
+app.engine('handlebars', handlebars());
+app.set('view engine', 'handlebars');
+app.set('views', __dirname + '/views');
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(logger('dev'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(session({ secret: 'keyboard cat',
+                  saveUninitialized: true,
+                  resave: true}));
+
+app.get('/', function(req, res){
+  res.render('index');
+});
+
+exports = module.exports = app;
