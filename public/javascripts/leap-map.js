d89aa6cec12d542dff707c1b37f0585b9437228e
tngyn <timngyn@gmail.com>
2015-06-04 15:17:49 -0700

    Changed map centering

--- public/javascripts/leap-map.js
+++ public/javascripts/leap-map.js
@@ -27,7 +27,7 @@ function initialize() {
   map = new google.maps.Map(d3.select("#map_canvas").node(), {
           zoom: 10,
           mapTypeId: google.maps.MapTypeId.ROADMAP,
-          center: new google.maps.LatLng(32.7157380, -117.1610840),
+          center: new google.maps.LatLng(32.893046, -117.147236),
           streetViewControl : false,
           styles: [{"featureType":"water","stylers":[{"visibility":"on"},{"color":"#b5cbe4"}]},{"featureType":"landscape","stylers":[{"color":"#efefef"}]},{"featureType":"road.highway","elementType":"geometry","stylers":[{"color":"#83a5b0"}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#bdcdd3"}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#ffffff"}]},{"featureType":"poi.park","elementType":"geometry","stylers":[{"color":"#e3eed3"}]},{"featureType":"administrative","stylers":[{"visibility":"on"},{"lightness":33}]},{"featureType":"road"},{"featureType":"poi.park","elementType":"labels","stylers":[{"visibility":"on"},{"lightness":20}]},{},{"featureType":"road","stylers":[{"lightness":20}]}],
           zoomControlOptions: {
